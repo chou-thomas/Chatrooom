a04e7f3695598920314eba8db5c52e01fc310f03
chou.thomas <chou.thomas3@gmail.com>
2016-02-05 18:44:37 -0800

    added users to the chatroom

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var names = [];
 
 server.listen(8000);
 console.log('listening on port 8000');
@@ -14,8 +15,34 @@ app.get('/', function(req,res){
 
 // allows client to connect to socket.io
 io.sockets.on('connection', function(socket){
+	
+	function updateNames(){
+		io.sockets.emit('usernames', names);
+	}
+
+	socket.on('sending_user', function(data, callback){
+		// if statement that checks if the index in names array does not equal -1, proves if name exists
+		if (names.indexOf(data) != -1){ 
+			callback(false);
+		} else{
+		// else allows to store each user in socket, and push into names array
+			callback(true);
+			socket.name = data;
+			names.push(socket.name);
+			updateNames();
+
+		}
+	});
+
 	socket.on('sending_message', function(data){
-		io.sockets.emit('new message', data);
-		// socket.broadcast.emit('new message', data);
+		io.sockets.emit('new_message', {msg: data, name: socket.name});
+	});
+	
+	socket.on('disconnect', function(data){
+		if(!socket.name) return;
+		// splices the elements in the array to allow users to disconnect
+		names.splice(names.indexOf(socket.name), 1);
+		console.log(names);
+		updateNames();
 	});
 });
