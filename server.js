cf51ae42c9498f8f2a714ca3816c20861f06b270
chou.thomas <chou.thomas3@gmail.com>
2016-02-08 11:43:28 -0800

    added private messaging

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var names = [];
+var names = {};
 
 server.listen(8000);
 console.log('listening on port 8000');
@@ -17,31 +17,49 @@ app.get('/', function(req,res){
 io.sockets.on('connection', function(socket){
 	
 	function updateNames(){
-		io.sockets.emit('usernames', names);
+		io.sockets.emit('usernames', Object.keys(names));
 	}
 
 	socket.on('sending_user', function(data, callback){
-		// if statement that checks if the index in names array does not equal -1, proves if name exists
-		if (names.indexOf(data) != -1){ 
+		// if statement that checks through the newly changed names object. 
+		if (data in names){ 
 			callback(false);
 		} else{
 		// else allows to store each user in socket, and push into names array
 			callback(true);
 			socket.name = data;
-			names.push(socket.name);
+			names[socket.name] = socket;
 			updateNames();
 
 		}
 	});
 
-	socket.on('sending_message', function(data){
-		io.sockets.emit('new_message', {msg: data, name: socket.name});
-	});
+	socket.on('sending_message', function(data, callback){
+		var msg = data.trim();
+		if(msg.substr(0,5) === '/msg '){
+			msg = msg.substr(5);
+			var index = msg.indexOf(' ');
+			if(index !== -1){
+					var person = msg.substring(0, index);
+					var msg =  msg.substring(index +1);
+					if(person in names){
+					names[person].emit('private_msg', {msg: msg, name: socket.name});	
+					console.log("msg");	
+			} else{
+				callback('Enter a person who is online');
+				}
+			} else{
+				callback("Please send a message for your private message");
+			}
+		} else{
+				io.sockets.emit('new_message', {msg: msg, name: socket.name});
+				}
+			});
 	
 	socket.on('disconnect', function(data){
 		if(!socket.name) return;
 		// splices the elements in the array to allow users to disconnect
-		names.splice(names.indexOf(socket.name), 1);
+		delete names[socket.name];
 		console.log(names);
 		updateNames();
 	});
